5b9878c2f133511cfa3febd67071e7afc56c10e2
Zpryakhin <zpryakhin2@yandex.ru>
2020-10-24 21:28:02 +0300

    Update Rabin_Karp.py

--- Rabin_Karp.py
+++ Rabin_Karp.py
@@ -3,11 +3,13 @@ p = input("Введите подстроку\n")
 def find_all_rabin_karp(text, pattern):
     result = []
     patternsum = sum(ord(s) for s in pattern)
+    textwindowsum = sum(ord(text[j]) for j in range(len(pattern)))
     for i in range(len(text) - len(pattern) + 1):
-        textwindowsum = sum(ord(text[j+i]) for j in range(len(pattern)))
+        textwindowsum = textwindowsum - ord(text[i])+ord(text[i+len(pattern)-1])  
         if patternsum == textwindowsum:
             if text.startswith(pattern, i):
                 result.append(i)
+              
     return result        
 
 print(find_all_rabin_karp(t, p))
