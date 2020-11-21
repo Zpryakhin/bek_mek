6d1e5f642ea26612d800d5574a29c04b9bd0da55
Zpryakhin <zpryakhin2@yandex.ru>
2020-11-22 00:41:08 +0300

    Update fractial.py

--- fractial.py
+++ fractial.py
@@ -3,29 +3,23 @@ import turtle as tl
 from turtle import Turtle
 tl = Turtle()
 tl.speed(1000)
-import turtle as tl
-
-
-def draw_fractal(size):
-    if size >= 5:	    
-        draw_fractal(size / 3.0)	 
-        tl.left(60)	        
-        draw_fractal(size / 3.0)	 
-        tl.right(120)	    
-        draw_fractal(size / 3.0)	
-        tl.left(60)	
-        draw_fractal(size / 3.0)
-    else:	   
-        tl.forward(size)	
-
-
-size = 200	
+def draw_fractal(scale):
+    if scale >= 5:
+        tl.forward(scale)
+        tl.right(60)
+        draw_fractal(scale/2)
+        tl.left(60)
+        draw_fractal(scale/1.5)
+        tl.left(60)
+        draw_fractal(scale/2)
+        tl.right(60)
+        tl.backward(scale)
+scale = 100
+tl.left(90)
+tl.pencolor('green')
 tl.pensize(2)
-tl.penup()	
-tl.goto(-size, -size/4)	
-tl.pendown()	
-
 for i in range(6):
-    draw_fractal(size)
-    tl.right(60)	
-tl.done()	
+    draw_fractal(scale)
+    tl.right(60)
+tl.done()
+    
