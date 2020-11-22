185df282b84df931ee91905623ee6c8b107771dc
Zpryakhin <zpryakhin2@yandex.ru>
2020-11-22 23:00:28 +0300

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,8 +1,10 @@
-a = int(input('Введите первое число '))
-b = int(input('Введите второе число '))
-while a != 0 and b != 0:
-    if a > b:
-        a = a % b
-    else:
-        b = b % a
-print(a + b)		   	
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+def gcd(a,b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return (a + b)
+print(gcd(a,b))        		   	
