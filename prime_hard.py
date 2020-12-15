a5f63ea7d136ba6fbbce06501a905480b0fb6c21
Zpryakhin <zpryakhin2@yandex.ru>
2020-12-15 23:53:15 +0300

    Update prime_hard.py

--- prime_hard.py
+++ prime_hard.py
@@ -1,11 +1,15 @@
 import math
+def Is_Prime(a):
+    ans = False
+    b = math.factorial(a-1)
+    #По теореме Вильсона, если p простое, то (p-1)! = -1 mod p или (p-1)! + 1 = 0 mod p
+    if (b + 1) % a == 0:
+        ans = True
+    return ans
 a = int(input("Введите число: "))
-ans = False
-b = math.factorial(a-1)
-#По теореме Вильсона, если p простое, то (p-1)! = -1 mod p или (p-1)! + 1 = 0 mod p
-if (b + 1) % a == 0:
-    ans = True
-if ans:
-    print("Число ", a, " простое")
-else:
-    print("Число ", a, " составное")  
+#if Is_Prime(a):
+#    print("Простое")
+#else:
+#    print("Составное") 
+print(Is_Prime(a))   
+
