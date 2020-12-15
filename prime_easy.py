1fceac9ca1ed131b3e4104d84ada0e9e575f8948
Zpryakhin <zpryakhin2@yandex.ru>
2020-12-15 23:53:21 +0300

    Update prime_easy.py

--- prime_easy.py
+++ prime_easy.py
@@ -1,14 +1,17 @@
 import math
-a = int(input("Введите число: "))
-ans = True
-b = round(math.sqrt(a))
-i = 2
-while i <= b:
-    if a % (i) == 0:
-        ans = False
-        break
-    i = i + 1
-if ans:
-    print("Число ", a, " простое")
-else:
-    print("Число ", a, " составное")        
+def Is_Prime(a):
+    ans = True
+    b = round(math.sqrt(a))
+    i = 2
+    while i <= b:
+        if a % (i) == 0:
+            ans = False
+            break
+        i += 1
+    return ans    
+a = int(input("Введите число: "))    
+#if Is_Prime(a):
+#    print("Число ", a, " простое")
+#else:
+#    print("Число ", a, " составное")
+print(Is_Prime(a))        
